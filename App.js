2b9ebf4a346170dd32363cae7ac2520830a98277
deepthina <73054651+deepthina@users.noreply.github.com>
2022-01-09 17:44:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false,
+    };
+  }
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold,
+    });
+    this.setState({
+      fontLoaded: true,
+    });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+  render() {
+    if (this.state.fontLoaded === true) return <BottomTabNavigator />;
+    else return null;
+  }
+}
---
+++ screens/Transaction.js
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default class TransactionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: "",
+    };
+  }
+
+  getCameraPermissions = async (domState) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+      hasCameraPermissions: status === "granted",
+      domState: domState,
+      scanned: false,
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true,
+    });
+  };
+
+  render() {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+    if (domState === "scanner") {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.lowerContainer}>
+          <View style={styles.textinputContainer}>
+            <TextInput />
+            <TouchableOpacity style={styles.scanbutton}>
+              <Text style={styles.scanbuttonText}>Scan</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={[styles.textinputContainer, { marginTop: 25 }]}>
+            <TextInput />
+            <TouchableOpacity style={styles.scanbutton}>
+              <Text style={styles.scanbuttonText}>Scan</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  upperContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appIcon: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 80,
+  },
+  appName: {
+    width: 80,
+    height: 80,
+    resizeMode: "contain",
+  },
+  lowerContainer: {
+    flex: 0.5,
+    alignItems: "center",
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF",
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
+    color: "#FFFFFF",
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scanbuttonText: {
+    fontSize: 24,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold",
+  },
+});
